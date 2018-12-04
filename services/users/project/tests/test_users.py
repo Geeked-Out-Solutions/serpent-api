4eb31da275d1e6fef92f0fbd823981e393912d60
btotharye <git email>
2018-12-04 18:56:01 -0500

    working tests for error handling

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -1,14 +1,23 @@
 import json
 import unittest
 
+from project import db
+from project.api.models import User
 from project.tests.base import BaseTestCase
+
+
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
 
 
 class TestUserService(BaseTestCase):
     """Tests for the Users Service."""
 
     def test_users(self):
-        """Ensure the /ping route behaves correctly"""
+        """Ensure the /ping route behaves correctly."""
         response = self.client.get('/users/ping')
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
@@ -83,6 +92,53 @@ class TestUserService(BaseTestCase):
             self.assertIn(
                 'Sorry. That email already exists.', data['message'])
             self.assertIn('fail', data['status'])
+
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = add_user('monty', 'monty@python.org')
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('monty', data['data']['username'])
+            self.assertIn('monty@python.org', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+
+    def test_all_users(self):
+        """Ensure get all users behaves correctly."""
+        add_user('monty', 'monty@python.org')
+        add_user('vader', 'vader@starwarz.com')
+        with self.client:
+            response = self.client.get('/users')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 2)
+            self.assertIn('monty', data['data']['users'][0]['username'])
+            self.assertIn(
+                'monty@python.org', data['data']['users'][0]['email'])
+            self.assertIn('vader', data['data']['users'][1]['username'])
+            self.assertIn(
+                'vader@starwarz.com', data['data']['users'][1]['email'])
+            self.assertIn('success', data['status'])
 
 if __name__ == '__main__':
     unittest.main()
