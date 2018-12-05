f3e474872e14727f33862032f43c7ea5482316f8
btotharye <git email>
2018-12-05 15:10:45 -0500

    user auth setup with jwt

--- services/serpenttracker/project/tests/test_config.py
+++ services/serpenttracker/project/tests/test_config.py
@@ -26,6 +26,8 @@ class TestDevelopmentConfig(TestCase):
         )
         self.assertTrue(app.config['DEBUG_TB_ENABLED'])
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
 
 
 class TestTestingConfig(TestCase):
@@ -34,7 +36,8 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'serpent_key')
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
@@ -43,6 +46,8 @@ class TestTestingConfig(TestCase):
         )
         self.assertFalse(app.config['DEBUG_TB_ENABLED'])
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 4)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 0)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 3)
 
 
 class TestProductionConfig(TestCase):
@@ -51,10 +56,13 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_production(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'serpent_key')
+        self.assertTrue(
+            app.config['SECRET_KEY'] == os.environ.get('SECRET_KEY'))
         self.assertFalse(app.config['TESTING'])
         self.assertFalse(app.config['DEBUG_TB_ENABLED'])
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 13)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_DAYS'] == 30)
+        self.assertTrue(app.config['TOKEN_EXPIRATION_SECONDS'] == 0)
 
 
 if __name__ == '__main__':
