21167811de918bd6436a262732c832353e6b15af
btotharye <git email>
2018-12-08 09:19:09 -0500

    testing elb with travis

--- docker-push.sh
+++ docker-push.sh
@@ -3,6 +3,16 @@
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
 
+  if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
+    export DOCKER_ENV=stage
+    export REACT_APP_USERS_SERVICE_URL="http://serpenttracker-staging-alb-155647854.us-east-1.elb.amazonaws.com"
+  elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
+    export DOCKER_ENV=prod
+    # export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-1112328201.us-east-1.elb.amazonaws.com"
+    # export DATABASE_URL="$AWS_RDS_URI"
+    # export SECRET_KEY="$PRODUCTION_SECRET_KEY"
+  fi
+
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
@@ -28,7 +38,7 @@ then
     docker tag $SERPENTAPI_DB:$COMMIT $REPO/$SERPENTAPI_DB:$TAG
     docker push $REPO/$SERPENTAPI_DB:$TAG
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_SERPENTAPI_SERVICE_URL=TBD
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_SERPENT_SERVICE_URL=$REACT_APP_SERPENT_SERVICE_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
