f31ed3f9afcdefcdaf8a1eceebb045f34d9f1dd4
btotharye <git email>
2018-12-09 16:31:36 -0500

    updating tests

--- services/client/src/components/__tests__/About.test.jsx
+++ services/client/src/components/__tests__/About.test.jsx
@@ -14,5 +14,5 @@ test('About renders properly', () => {
   const wrapper = shallow(<About/>);
   const element = wrapper.find('p');
   expect(element.length).toBe(1);
-  expect(element.text()).toBe('Add something relevant here.');
+  expect(element.text()).toBe('About information goes here');
 });
